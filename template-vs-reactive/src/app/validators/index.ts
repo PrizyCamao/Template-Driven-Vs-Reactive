de633c4834994a748e1df15a8fc748789c7e736d
PrizyCamao <101621152+PrizyCamao@users.noreply.github.com>
2022-03-15 14:33:34 +0100

    Deleted unnecessary Folder

--- template-vs-reactive/src/app/validators/index.ts
+++
@@ -1,1 +0,0 @@
-export { ValidatorModule } from './validators.module';
--- template-vs-reactive/src/app/validators/validation-messages.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface ValidationMessages {
-  [errorKey: string]: (...errorDetails: any[]) => string;
-}
-export const VALIDATION_MESSAGES = new InjectionToken<ValidationMessages>(
-  'VALIDATION_MESSAGES'
-);
